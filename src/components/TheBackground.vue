46ea3d0ad10b50f00ad8f5884594a909a639c774
Charon <chaosandpeace@outlook.com>
2026-04-14 17:39:13 +0800

    update global style

---
+++ src/components/TheBackground.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="geek-bg">
+    <div class="grid-layer"></div>
+    <div class="dot-layer"></div>
+  </div>
+</template>
+<style scoped>
+.geek-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  overflow: hidden;
+}
+.grid-layer{
+  position: absolute;
+  width: 200%;
+  height: 200%;
+  background-image: 
+    linear-gradient(var(--grid-color) 1px, transparent 1px),
+    linear-gradient(90deg, var(--grid-color) 1px, transparent 1px);
+  background-size: 50px 50px;
+  background-position: center;
+  opacity: 0.15;
+  transform: perspective(500px) rotateX(60deg);
+}
+</style>
